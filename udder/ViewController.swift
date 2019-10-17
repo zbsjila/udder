22abf97c37660f5e97a1a938c3c4f024c220e7ef
Bosheng Zhang <zbs.ustc@gmail.com>
2019-10-17 09:38:05 -0700

    Add label

---
+++ udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Bosheng Zhang on 10/17/19.
+//  Copyright © 2019 BZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
